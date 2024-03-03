58430334b10b1b66301343737b807c4b3c778aa0
ashwin.patti <ashwin.patti@servicenow.com>
2024-03-03 02:02:25 -0800

    standings

--- src/app/leagues/page.js
+++ src/app/leagues/page.js
@@ -9,23 +9,26 @@ export default function Home() {
     const [isLoading, setIsLoading] = useState(true);
     const [standings, setStandings] = useState([]);
 
-    useEffect(async () => {
-    try {
-        const restOperation = get({ 
-          apiName: 'fantasyapi',
-          path: '/league/standings/ZHVrZXMgaXBsIGZhbnRhc3kgMjAyNA==' 
-        });
-        const response = await restOperation.response;
-        console.log('GET call succeeded: ', response);
-        setIsLoading(false);
-        setStandings(response.data.standings);
-      } catch (e) {
-        console.log('GET call failed: ', JSON.parse(e.response.body));
-      }
+    const getStandings = async () => {
+        try {
+            const restOperation = get({ 
+              apiName: 'fantasyapi',
+              path: '/league/standings/ZHVrZXMgaXBsIGZhbnRhc3kgMjAyNA==' 
+            });
+            const response = await restOperation.response;
+            console.log('GET call succeeded: ', response);
+            setIsLoading(false);
+            setStandings(response.standings);
+          } catch (e) {
+            console.log('GET call failed: ', e);
+          }
+    }
+    useEffect( () => {
+        getStandings();
     }, []);
+
     return(
         <Flex>
-
             {(isLoading) && <Loader width="5rem" height="5rem"/>}
         </Flex>
       );
